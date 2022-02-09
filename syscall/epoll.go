50fd85fabcca883750e32fef5a2a3daf1634c0cd
Nugraha <26342994+ii64@users.noreply.github.com>
2022-02-10 02:16:53 +0700

    feat(syscall): add madvise, epollctl
    Signed-off-by: Nugraha <26342994+ii64@users.noreply.github.com>

--- syscall/epoll.go
+++ syscall/epoll.go
@@ -2,6 +2,7 @@ package syscall
 
 import (
 	"syscall"
+	"unsafe"
 
 	"github.com/ii64/gouring"
 )
@@ -9,7 +10,10 @@ import (
 var _ = syscall.EpollCtl
 
 // EpollCtl
-func EpollCtl(sqe *gouring.SQEntry, epfd int, op int, fd int, event *syscall.EpollEvent) (err error) {
+func EpollCtl(sqe *gouring.SQEntry, epfd int, op int, fd int, event *syscall.EpollEvent) {
 	sqe.Opcode = gouring.IORING_OP_EPOLL_CTL
-	return nil
+	sqe.Fd = int32(epfd)
+	*sqe.Addr() = uint64(uintptr(unsafe.Pointer(event)))
+	sqe.Len = uint32(op)
+	*sqe.Offset() = uint64(fd)
 }
--- syscall/helper.go
+++ syscall/helper.go
@@ -5,5 +5,7 @@ import (
 	_ "unsafe"
 )
 
+var _zero uintptr
+
 //go:linkname anyToSockaddr syscall.anyToSockaddr
 func anyToSockaddr(rsa *syscall.RawSockaddrAny) (syscall.Sockaddr, error)

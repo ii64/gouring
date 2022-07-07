7fa3c507b1df78eac871dae52c36f83a48eb3c48
Nugraha <richiisei@gmail.com>
2022-07-07 08:26:44 +0700

    api: impl get sqe, wait cqe, and test
    Signed-off-by: Nugraha <richiisei@gmail.com>

--- uring.go
+++ uring.go
@@ -2,14 +2,16 @@ package gouring
 
 func New(entries uint32, flags uint32) (*IoUring, error) {
 	ring := &IoUring{}
-	err := io_uring_queue_init(entries, ring, flags)
+	p := new(IoUringParams)
+	p.Flags = flags
+	err := io_uring_queue_init_params(entries, ring, p)
 	if err != nil {
 		return nil, err
 	}
 	return ring, nil
 }
 
-func NewWithParamms(entries uint32, params *IoUringParams) (*IoUring, error) {
+func NewWithParams(entries uint32, params *IoUringParams) (*IoUring, error) {
 	ring := &IoUring{}
 	err := io_uring_queue_init_params(entries, ring, params)
 	if err != nil {
@@ -20,4 +22,20 @@ func NewWithParamms(entries uint32, params *IoUringParams) (*IoUring, error) {
 
 func (h *IoUring) Close() {
 	h.io_uring_queue_exit()
+}
+
+func (h *IoUring) GetSQE() *IoUringSqe {
+	return h.io_uring_get_sqe()
+}
+
+func (h *IoUring) WaitCQE(cqePtr **IoUringCqe) error {
+	return h.io_uring_wait_cqe(cqePtr)
+}
+
+func (h *IoUring) Submit() (int, error) {
+	return h.io_uringn_submit()
+}
+
+func (h *IoUring) SubmitAndWait(waitNr uint32) (int, error) {
+	return h.io_uring_submit_and_wait(waitNr)
 }
